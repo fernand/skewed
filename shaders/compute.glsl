0f4bcbb71118aa6c0939a76d2d54f39fb803ee16
Fernand Pajot <fernand@epigram.me>
2019-08-26 17:44:28 -0700

    Moved to simple ray marching with SDFs

--- shaders/compute.glsl
+++ shaders/compute.glsl
@@ -12,63 +12,63 @@ layout(std430, binding = 0) readonly buffer data
 };
 
 const float PI = 3.1415926535897932384626433832795;
-const int NUM_ITER = 10000;
-const float STEP = 0.05;
-const float BOX_R2 = 100.0;
 
-const float SPH_R = 0.5;
-const vec3 SPH_1 = vec3(0., 0., 1.0);
-const vec3 SPH_2 = vec3(0., 0., -1.0);
-const vec4 RED = vec4(1., 0., 0., 1.);
-const vec4 GREEN = vec4(0., 1., 0., 1.);
+const vec3 RED = vec3(1., 0., 0.);
+const vec3 GREEN = vec3(0., 1., 0.);
+const vec3 unitBox = vec3(0.5, 0.5, 0.5);
+
+float sdSphere(vec3 p, float r) {
+    return length(p) - r;
+}
+
+float sdBox(vec3 p, vec3 b) {
+    vec3 d = abs(p) - b;
+    return length(max(d, 0.0)) + min(max(d.x, max(d.y, d.z)), 0.0);
+}
+
+vec4 opU(vec4 d1, vec4 d2) {
+    return (d1.x < d2.x) ? d1 : d2;
+}
+
+vec4 map(in vec3 pos) {
+    vec4 res = vec4(1e10, 0.0, 0.0, 0.0);
+    res = opU(res, vec4(sdBox(pos - vec3(0.0, 0.0, 0.0), unitBox), RED));
+    res = opU(res, vec4(sdBox(pos - vec3(0.0, 0.0, -2.0), unitBox), RED));
+    res = opU(res, vec4(sdBox(pos - vec3(0.0, 0.0, -4.0), unitBox), RED));
+    res = opU(res, vec4(sdBox(pos - vec3(-2.0, 0.0, 0.0), unitBox), RED));
+    res = opU(res, vec4(sdBox(pos - vec3(-2.0, 0.0, -2.0), unitBox), RED));
+    res = opU(res, vec4(sdBox(pos - vec3(-2.0, 0.0, -4.0), unitBox), RED));
+    res = opU(res, vec4(sdBox(pos - vec3(2.0, 0.0, 0.0), unitBox), RED));
+    res = opU(res, vec4(sdBox(pos - vec3(2.0, 0.0, -2.0), unitBox), RED));
+    res = opU(res, vec4(sdBox(pos - vec3(2.0, 0.0, -4.0), unitBox), RED));
+    return res;
+}
+
+const float tMax = 15.0;
 
 void main() {
     vec4 color = vec4(0.0, 0.0, 0.0, 1.0);
     float nx = nxNyHalfHeight.x;
     float ny = nxNyHalfHeight.y;
     float halfHeight = nxNyHalfHeight.z;
-    vec3 origin = eye.xyz;
+    vec3 rayOrigin = eye.xyz;
     float halfWidth = halfHeight * float(nx) / ny;
     float s = float(gl_GlobalInvocationID.x) / nx;
     float t = float(gl_GlobalInvocationID.y) / ny;
 
-    vec3 lowerLeftCorner = origin - halfWidth * u.xyz - halfHeight * v.xyz - w.xyz;
+    vec3 lowerLeftCorner = rayOrigin - halfWidth * u.xyz - halfHeight * v.xyz - w.xyz;
     vec3 horizontal = 2.0 * halfWidth * u.xyz;
     vec3 vertical = 2.0 * halfHeight * v.xyz;
+    vec3 rayDir = normalize(lowerLeftCorner + s * horizontal + t * vertical - rayOrigin);
 
-    vec3 direction = normalize(lowerLeftCorner + s * horizontal + t * vertical - origin);
-    vec3 point = origin;
-    float distTraced = 0.0;
-    float nextTurnDist = 2.0 * sqrt(2.0);
-    int numTurns = 0;
-    float prevSqrNorm;
-    float sqrNorm = dot(point, point);
-
-    for (int i=0; i<NUM_ITER; i++) {
-        if (nextTurnDist - distTraced < 0.1f) {
-            if (numTurns == 0) {
-                direction = cross(direction, cUp.xyz);
-            } else {
-                direction = - cross(direction, cUp.xyz);
-            }
-            nextTurnDist += 1.0;
-            if (numTurns == 3) {
-                numTurns = 0;
-            }
-        }
-        point += STEP * direction;
-        sqrNorm = dot(point, point);
-        distTraced += STEP;
-        
-        if (sqrNorm > BOX_R2) {
-            break;
-        } else if (length(point - SPH_1) <= SPH_R) {
-            color = RED;
-            break;
-        } else if (length(point - SPH_2) <= SPH_R) {
-            color = GREEN;
+    t = 0.0;
+    for (int i=0; i<70 && t<tMax; i++) {
+        vec4 hit = map(rayOrigin + t * rayDir);
+        if (abs(hit.x) < (0.0001 * t)) {
+            color = vec4(hit.yzw, 1.0);
             break;
         }
+        t += hit.x;
     }
     imageStore(pixels, ivec2(gl_GlobalInvocationID.xy), color);
 }
